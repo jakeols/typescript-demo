00e8f912ed7423b20c84a35884aeafec0bfb9d6b
jakeols <jake_ols@live.com>
2019-12-03 09:08:30 -0800

    add primefinder code

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import primefinder from "./primefinder";
 
-primefinder(1);
+console.log(primefinder(13));
--- primefinder.ts
+++ primefinder.ts
@@ -2,6 +2,17 @@
  * Export our default PrimeFinder function here
  * @param num
  */
-export default function (num: number) {
-    console.log("testing");
+export default function(num: number) {
+  if (num === 1) {
+    return false;
+  } else if (num === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < num; x++) {
+      if (num % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
